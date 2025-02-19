3f2cda9754e2c2ee5a8b43d1510c1553e8e08301
Erick-Jara <erick.ajm1993@gmail.com>
2025-02-19 00:46:27 -0500

    subiendo primer funcion

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 from fastapi import FastAPI
 
 import pandas as pd
-import unidecode  # Para manejar tildes
+
 
 app = FastAPI()
 
-@app.get("/") 
+# Cargar dataset
+df = pd.read_csv("datos/dataset.csv")
+
+def obtener_meses():
+   
+    meses = {
+        "Enero": "January", "Febrero": "February", "Marzo": "March", "Abril": "April",
+        "Mayo": "May", "Junio": "June", "Julio": "July", "Agosto": "August",
+        "Septiembre": "September", "Octubre": "October", "Noviembre": "November", "Diciembre": "December"
+    }
+    return meses
+
+
+@app.get("/cantidad_filmaciones_mes") 
 def cantidad_filmaciones_mes(Mes):
     
     try:
-        # Cargar dataset
-        df = pd.read_csv("datos/dataset.csv")
+        
+        # Obtener diccionario de traducción
+        meses_traducidos = obtener_meses()
 
-        # Normalizar mes (quitar tildes y convertir a minúsculas)
-        mes_normalizado = unidecode.unidecode(Mes.strip().lower())
-
-        # Validar si el mes ingresado es correcto
-        if mes_normalizado not in meses_traducidos:
-            return "Error: Mes inválido. Usa un mes en español, por ejemplo: 'Enero', 'Febrero'."
+        # (convertir primera letra en mayúscula, resto en minúscula)
+        mes_formateado = mes.capitalize()
 
         # Obtener el nombre del mes en inglés
-        mes_ingles = meses_traducidos[mes_normalizado]
+        mes_ingles = meses_traducidos[mes_formateado]
 
         # Contar cuántas películas fueron estrenadas en ese mes
         cantidad = df[df['month'] == mes_ingles].shape[0]
 
-        return f"{cantidad} cantidad de películas fueron estrenadas en el mes de {Mes.capitalize()}."
-
-    except FileNotFoundError:
-        return "Error: No se encontró el archivo 'dataset.csv'. Verifica la ruta y el nombre del archivo."
-
-    except pd.errors.EmptyDataError:
-        return "Error: El archivo 'dataset.csv' está vacío."
-
-    except KeyError:
-        return "Error: La columna 'month' no existe en el dataset."
+        return f"{cantidad} cantidad de películas fueron estrenadas en el mes de {mes_formateado}."
 
     except Exception as e:
         return f"Error inesperado: {str(e)}"
